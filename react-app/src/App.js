87d3f5b16bf07d6721663859a1cb8e09a64f7d54
f1ckl3p1ckl3 <lattetehkat@gmail.com>
2025-07-01 21:36:01 +1000

    Add teacake recipe

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -29,7 +29,10 @@ function App() {
           <Route path='/' element={<HomePage />} />
             {/* load in all the recipes */}
             {recipesArray.map((recipe) => (
-            <Route path={recipe.url} element={<RecipePage title={recipe.title} image={`images/${recipe.image}`} ingredients={recipe.ingredients} method={recipe.method} summary={recipe.summary} extendedSummary={recipe.extendedSummary} source={recipe.source} serves={recipe.serves}/>} />
+            <Route path={recipe.url} element={<RecipePage title={recipe.title} 
+            image={`images/${recipe.image}`} ingredients={recipe.ingredients} method={recipe.method} 
+            summary={recipe.summary} extendedSummary={recipe.extendedSummary} 
+            source={recipe.source} serves={recipe.serves}/>} />
           ))}
         </Routes>
     </div>
